ea907e6f79802d9818309b3df2d2e7ae7a4d21f8
Andrew Park <andysampark@gmail.com>
2022-07-07 12:12:49 -0400

    build

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,21 @@ const path = require("path")
 const express = require("express")
 const colors = require("colors")
 const mongoose = require("mongoose")
-const dotenv = require("dotenv").config({path: __dirname + "/.env"})
 const cors = require("cors")
 const connectDB = require("./db/db")
 const port = process.env.PORT || 5000
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: __dirname + '/.env' });
+} else {
+    const dotenv = require("dotenv").config()
+}
 
-connectDB()
+
+console.log(process.env.MONGO_URI)
+
+
+// connectDB()
 
 const app = express();
 app.use(cors());
@@ -16,19 +24,27 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
+
 app.use("/api/posts", require("./routes/postRoute"))
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/build')));
     app.get('/*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+        // res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, '../', 'frontend', 'build', 'index.html'))
     );
 } else {
     app.get('/', (req, res) => res.send('Please set to production'));
 }
 
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`listening on port ${port}`)
+// })
