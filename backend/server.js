df3baed6cf5e2e2e3c956eb591c6b8ed0a82c67e
Andrew Park <andysampark@gmail.com>
2022-07-06 21:07:43 -0400

    changed node_env to build production

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/", require("./routes/postRoute"))
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+}
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
