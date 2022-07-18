2e74f1edc60bb11d55808f7ecc5332a438a26b8a
Andrew Park <andysampark@gmail.com>
2022-07-18 12:54:24 -0400

    change order, added google analytics

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -24,7 +24,7 @@ function PostList() {
         <>
             <div className='box-border grid place-items-stretch sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 md:gap-6 lg:gap-10 gap-y-6'>
                 {/* <div className='mx-auto sm:columns-1 md:columns-2 lg:columns-3 gap-7'> */}
-                {postList.map(post => {
+                {postList.slice(0).reverse().map(post => {
                     return <Post post={post} key={post._id} className="inline-block box-border"></Post>
                 })}
             </div>
